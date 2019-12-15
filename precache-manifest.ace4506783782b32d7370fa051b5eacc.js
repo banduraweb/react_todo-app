b1cd0a8d29cb40596e933b4cb35cf72fd472d7bb
bandura <banduraweb@ukr.net>
2019-12-15 20:26:37 +0200

    Updates

---
+++ precache-manifest.ace4506783782b32d7370fa051b5eacc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416d2b75aebdf85109b4a0d6e92d4b55",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "9f92b5efe96d712c11f9",
+    "url": "/react_todo-app/static/css/main.a8e92d56.chunk.css"
+  },
+  {
+    "revision": "12778716995bb30f3683",
+    "url": "/react_todo-app/static/js/2.4e881a18.chunk.js"
+  },
+  {
+    "revision": "9f92b5efe96d712c11f9",
+    "url": "/react_todo-app/static/js/main.b9d6b4b1.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
