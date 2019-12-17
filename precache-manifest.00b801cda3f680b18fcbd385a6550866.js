d9c511ea229a1863d3e4c6da3818a9a28740662b
bandura <banduraweb@ukr.net>
2019-12-17 11:38:49 +0200

    Updates

--- precache-manifest.00b801cda3f680b18fcbd385a6550866.js
+++ precache-manifest.00b801cda3f680b18fcbd385a6550866.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "faf494a214aba21f6869079de66a3287",
+    "revision": "f2790255eee063c74dd7d21bfb8935c6",
     "url": "/react_todo-app/index.html"
   },
   {
-    "revision": "2b9d47ffcc21d6ed1a00",
+    "revision": "7419e0d75551b6504b80",
     "url": "/react_todo-app/static/css/main.abae98c4.chunk.css"
   },
   {
-    "revision": "0814fc003ec750e34d00",
-    "url": "/react_todo-app/static/js/2.d1d0235a.chunk.js"
+    "revision": "12778716995bb30f3683",
+    "url": "/react_todo-app/static/js/2.4e881a18.chunk.js"
   },
   {
-    "revision": "2b9d47ffcc21d6ed1a00",
-    "url": "/react_todo-app/static/js/main.7666501b.chunk.js"
+    "revision": "7419e0d75551b6504b80",
+    "url": "/react_todo-app/static/js/main.d7d90226.chunk.js"
   },
   {
     "revision": "51af3b3c9c5f8ccb1c53",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_todo-app/precache-manifest.db897b302808b74fdb20c1b8898fdd4f.js"
+  "/react_todo-app/precache-manifest.00b801cda3f680b18fcbd385a6550866.js"
 );
 
 self.addEventListener('message', (event) => {
